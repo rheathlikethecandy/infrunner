8e4213ea08bfd50ef0bdac8a2058ce72fc48611d
Ryan Heath <rheath@ucsc.edu>
2020-04-26 13:30:45 -0700

    oops

--- src/main.js
+++ src/main.js
@@ -2,13 +2,8 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [ Menu, Play ]
+    scene: [****, ****]
 }
 let game = new Phaser.Game(config);
-//define game settings
-game.settings = {
-    spaceshipSpeed: 3,
-    gameTimer: 60000    
-}
 //reserve keyboard vars
-let keyUP, keyLEFT, keyRIGHT, keyA, keyD, keyW;
+let keyUP, keyLEFT, keyRIGHT, keyDOWN, keyA, keyD, keyW, keyS;
