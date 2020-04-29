212bb354b57779cfc1d9fcb76d355ab0b5b4e4fa
Ryan Heath <rheath@ucsc.edu>
2020-04-29 16:40:58 -0700

    .

--- prefabs/Player.js
+++ prefabs/Player.js
@@ -1,15 +1,15 @@
 //Player prefab
 class Player extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
-      super(scene, x, y, texture, frame);
-  
-      //add object to existing scene
-      scene.add.existing(this);
-      //action state
-      this.isJump = false;
-      this.isSlide = false;
+        super(scene, x, y, texture, frame);
 
-      this.sfxRocket = scene.sound.add('sfx_rocket'); //add rocket sfx
+        //add object to existing scene
+        scene.add.existing(this);
+        //action state
+        this.isJump = false;
+        this.isSlide = false;
+        this.hasDjump = true;
+        this.sfxRocket = scene.sound.add('sfx_rocket'); //add rocket sfx
     }
     update() {
         if(keyUP.isDown || keyW.isDown) {
